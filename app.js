1cd90f1c663b6158e37c85c423381473fc855212
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-06 13:56:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogRouter = require('./routes/dog');
+var addmodsRouter = require('./routes/addmods');
+
 
 
 var app = express();
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dog', dogRouter);
+app.use('/addmods', addmodsRouter);
+
 
 
 
